f9949b0e4262c463ad48dbe0ca8d17a1164ad6ba
wmilou <wedsleyfernando@gmail.com>
2023-06-10 21:44:03 -0300

    updating readme file, and updating the database script...

--- infra/Database/migration.sql
+++ infra/Database/migration.sql
@@ -1,66 +1,59 @@
 CREATE DATABASE travelapp;
 \c travelapp 
--- CreateTable
-CREATE TABLE "Pessoa" (
-    "id_pessoa" SERIAL NOT NULL,
-    "nome" TEXT NOT NULL,
-    "dat_nasc" TEXT NOT NULL,
-    "rg" TEXT NOT NULL,
-    "cpf_cnpj" TEXT NOT NULL,
-    "telefone" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "senha" TEXT NOT NULL,
-    "ativo" BOOLEAN NOT NULL,
 
-    CONSTRAINT "Pessoa_pkey" PRIMARY KEY ("id_pessoa")
+CREATE TABLE pessoa (
+    id_pessoa serial NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    dat_nasc VARCHAR(50) NOT NULL,
+    rg VARCHAR(50) NOT NULL,
+    cpf_cnpj VARCHAR(50) NOT NULL,
+    telefone VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    senha VARCHAR(50) NOT NULL,
+    ativo BOOLEAN NOT NULL,
+
+    CONSTRAINT pessoa_pkey PRIMARY KEY (id_pessoa)
 );
 
--- CreateTable
-CREATE TABLE "Viagem" (
-    "id_viagem" SERIAL NOT NULL,
-    "local" TEXT NOT NULL,
-    "vagas" INTEGER NOT NULL,
-    "local_partida" TEXT NOT NULL,
-    "hora_partida" TEXT NOT NULL,
-    "hora_volta" TEXT NOT NULL,
-    "imagem_1" TEXT NOT NULL,
-    "imagem_2" TEXT NOT NULL,
-    "imagem_3" TEXT NOT NULL,
+CREATE TABLE viagem (
+    id_viagem SERIAL NOT NULL,
+    local VARCHAR(100) NOT NULL,
+    vagas INTEGER NOT NULL,
+    local_partida VARCHAR(300) NOT NULL,
+    hora_partida VARCHAR(300) NOT NULL,
+    hora_volta VARCHAR(300) NOT NULL,
+    imagem_1 VARCHAR(999) NOT NULL,
+    imagem_2 VARCHAR(999) NOT NULL,
+    imagem_3 VARCHAR(999) NOT NULL,
+    viagem_cancelada BOOLEAN NOT NULL,
 
-    CONSTRAINT "Viagem_pkey" PRIMARY KEY ("id_viagem")
+    CONSTRAINT viagem_pkey PRIMARY KEY (id_viagem)
 );
 
--- CreateTable
-CREATE TABLE "Forma_pagamento" (
-    "id_pagamento" SERIAL NOT NULL,
-    "descricao" INTEGER NOT NULL,
-
-    CONSTRAINT "Forma_pagamento_pkey" PRIMARY KEY ("id_pagamento")
-);
-
--- CreateTable
-CREATE TABLE "Viagem_pessoa" (
-    "id_viagem_pessoa" SERIAL NOT NULL,
-    "fk_pessoa" INTEGER NOT NULL,
-    "fk_viagem" INTEGER NOT NULL,
-    "fk_forma_pag" INTEGER NOT NULL,
-    "sn_quitado" INTEGER NOT NULL DEFAULT 0,
-
-    CONSTRAINT "Viagem_pessoa_pkey" PRIMARY KEY ("id_viagem_pessoa")
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Pessoa_cpf_cnpj_key" ON "Pessoa"("cpf_cnpj");
-
--- AddForeignKey
-ALTER TABLE "Viagem_pessoa" ADD CONSTRAINT "Viagem_pessoa_fk_pessoa_fkey" FOREIGN KEY ("fk_pessoa") REFERENCES "Pessoa"("id_pessoa") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Viagem_pessoa" ADD CONSTRAINT "Viagem_pessoa_fk_viagem_fkey" FOREIGN KEY ("fk_viagem") REFERENCES "Viagem"("id_viagem") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Viagem_pessoa" ADD CONSTRAINT "Viagem_pessoa_fk_forma_pag_fkey" FOREIGN KEY ("fk_forma_pag") REFERENCES "Forma_pagamento"("id_pagamento") ON DELETE RESTRICT ON UPDATE CASCADE;
-
-
-INSERT INTO Viagem(local_de_viagem, vagas, local_partida, hora_partida, hora_volta, imagem_1, imagem_2, imagem_3, viagem_cancelada)
+INSERT INTO viagem(local, vagas, local_partida, hora_partida, hora_volta, imagem_1, imagem_2, imagem_3, viagem_cancelada)
 VALUES('Urubici', 5, 'Rodoviaria de Campinas', '07h00', '20h00', 'abc', 'abcd', 'abcde', false);
+
+CREATE TABLE forma_pagamento (
+    id_pagamento SERIAL NOT NULL,
+    descricao VARCHAR(999) NOT NULL,
+
+    CONSTRAINT forma_pagamento_pkey PRIMARY KEY (id_pagamento)
+);
+
+CREATE TABLE viagem_pessoa (
+    id_viagem_pessoa SERIAL NOT NULL,
+    fk_pessoa INTEGER NOT NULL,
+    fk_viagem INTEGER NOT NULL,
+    fk_forma_pag INTEGER NOT NULL,
+    sn_quitado INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT viagem_pessoa_pkey PRIMARY KEY (id_viagem_pessoa)
+);
+
+CREATE UNIQUE INDEX pessoa_cpf_cnpj_key ON pessoa(cpf_cnpj);
+
+ALTER TABLE viagem_pessoa ADD CONSTRAINT viagem_pessoa_fk_pessoa_fkey FOREIGN KEY (fk_pessoa) REFERENCES pessoa(id_pessoa) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE viagem_pessoa ADD CONSTRAINT viagem_pessoa_fk_viagem_fkey FOREIGN KEY (fk_viagem) REFERENCES viagem(id_viagem) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE viagem_pessoa ADD CONSTRAINT viagem_pessoa_fk_forma_pag_fkey FOREIGN KEY (fk_forma_pag) REFERENCES forma_pagamento(id_pagamento) ON DELETE RESTRICT ON UPDATE CASCADE;
